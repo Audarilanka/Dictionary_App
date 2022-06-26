18ccc4a835fd495de0efc91f4d4100318fd7a8d6
Audarilanka <dissanayakelanka96@gmail.com>
2022-06-26 12:51:34 +0530

    feat(project):dictionary App

---
+++ src/Pages/About/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+//import logo from "../components/part1/logo";
+
+export default function About() {
+  return (
+    <>
+      <div>
+        <p className="abot-paragraph">
+          dictionarylk.com provide millions of English definitions, spellings,
+          audio pronunciations, example sentences, and word origins.
+          dictionarylk.com’s main, proprietary source is the Random House
+          Unabridged Dictionary, which is continually updated by our team of
+          experienced lexicographers and supplemented with trusted, established
+          sources including American Heritage and Harper Collins to support a
+          range of language needs.
+        </p>
+
+        <p className="abot-paragraph">
+          Language is constantly evolving, and the editorial offerings from
+          Dictionary.com shine a light on how language is being used. From pop
+          culture to idioms and grammar advice, there’s always something new to
+          learn from lkdictionary.com.
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+//import React from "react";
+//import logo from "../../images/download-modified.png";
+export default function Contact() {
+  return (
+    <>
+      <div className="container3">
+        <div className="section32">
+          <div></div>
+        </div>
+        <div className="section33"></div>
+      </div>
+    </>
+  );
+}
