fd8c01b4d1944584bc2770ab883b1f1e9b80fcd4
Audarilanka <dissanayakelanka96@gmail.com>
2022-06-26 12:50:04 +0530

    feat(project):dictionary App

---
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function HeaderBar() {
+  return (
+    <div>
+      <span className="header">
+        <a
+          className="headerText"
+          href="https://www.google.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          dictionary.lk
+        </a>
+      </span>
+    </div>
+  );
+}
