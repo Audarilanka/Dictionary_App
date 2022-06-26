2d6520b404fb8ff27dd57fa598e132096812828d
Audarilanka <dissanayakelanka96@gmail.com>
2022-06-26 12:51:48 +0530

    feat(project):dictionary App

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,32 @@
+//import React from "react";
+import background from "../../images/book_stack.jpg";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <div>
+          <img src={background} alt="background" className="backgroundImage" />
+        </div>
+
+        <div className="searchBar">
+          <form className="form">
+            <label>
+              <span className="english"> English</span>
+              <input
+                type="text"
+                word="words"
+                placeholder="Search LK Advanced Learner’s Dictionary"
+                className="word"
+              />
+            </label>
+            <div>
+              <button className="search">Search </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
