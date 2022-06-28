19e8945f58b872dbb12f9fe104eda5183988fba2
Audarilanka <dissanayakelanka96@gmail.com>
2022-06-28 19:28:49 +0530

    feat(project)dictionary

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,13 +1,46 @@
-//import React from "react";
-//import logo from "../../images/download-modified.png";
+import "./Contact.css";
 export default function Contact() {
   return (
     <>
-      <div className="container3">
-        <div className="section32">
-          <div></div>
+      <div>
+        <div className="contact-background">
+          <p className="contactus">
+            <b>Contact Us</b>
+          </p>
+          <div className="contactbox">
+            <form>
+              <label>
+                <p>
+                  <input
+                    type="text"
+                    fullname="fullname"
+                    className="fullname"
+                    placeholder="Full Name"
+                  />
+                </p>
+                <p>
+                  <input
+                    type="text"
+                    email="email"
+                    className="email"
+                    placeholder="Email"
+                  />
+                </p>
+                <p>
+                  <input
+                    type="text"
+                    message="message"
+                    className="message"
+                    placeholder="Message"
+                  />
+                </p>
+              </label>
+            </form>
+          </div>
+          <button type="submit" className="button">
+            Submit
+          </button>
         </div>
-        <div className="section33"></div>
       </div>
     </>
   );
