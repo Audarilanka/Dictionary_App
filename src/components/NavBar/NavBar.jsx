8e31fbd88fba2d67e347408f7d2c36f1fe4196b8
Audarilanka <dissanayakelanka96@gmail.com>
2022-06-28 19:27:56 +0530

    feat(project)dictionary

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,8 @@
 import React    from "react";
 
 
+import "./NavBar.css";
+
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
@@ -8,16 +10,16 @@ export default function NavBar() {
     <div>
       <nav className="navbar">
         <Link to="/" className="nav-links">
-          <li className="nav-item">Dictionary</li>
+          <li className="nav-item1">Dictionary</li>
         </Link>
         <Link to="/abot" className="nav-links">
-          <li className="nav-item">Abot Us</li>
+          <li className="nav-item2">Abot Us</li>
         </Link>
         <Link to="/contact" className="nav-links">
-          <li className="nav-item">Contact</li>
+          <li className="nav-item3">Contact</li>
         </Link>
         <Link to="/help" className="nav-links">
-          <li className="nav-item">Help</li>
+          <li className="nav-item4">Help</li>
         </Link>
       </nav>
     </div>
