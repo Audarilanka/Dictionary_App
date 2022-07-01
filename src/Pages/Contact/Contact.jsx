de378df09cd25a52c6c4bcb8f43034755eb35ad2
Audarilanka <dissanayakelanka96@gmail.com>
2022-07-01 17:43:33 +0530

    feat(project)dictionary

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,5 +1,36 @@
+import React, { useState } from "react";
 import "./Contact.css";
+
 export default function Contact() {
+  const [values, setValues] = useState({
+    fullname: "",
+    email: "",
+    message: "",
+  });
+
+  const [submited, setSubmited] = useState(false);
+  const [vaild, setVaild] = useState(false);
+
+  const handleFullnameInputChange = (Event) => {
+    setValues({ ...values, fullname: Event.target.value });
+  };
+
+  const handleEmailInputChange = (Event) => {
+    setValues({ ...values, email: Event.target.value });
+  };
+
+  const handleMessageInputChange = (Event) => {
+    setValues({ ...values, message: Event.target.value });
+  };
+
+  const handleSubmit = (Event) => {
+    Event.preventDefault();
+    if (values.fullname && values.email && values.message) {
+      setVaild(true);
+    }
+    setSubmited(true);
+  };
+
   return (
     <>
       <div>
@@ -8,38 +39,62 @@ export default function Contact() {
             <b>Contact Us</b>
           </p>
           <div className="contactbox">
-            <form>
+            <form onSubmit={handleSubmit}>
+              {submited && vaild ? (
+                <div className="thanku">We got it.Thank you!</div>
+              ) : null}
               <label>
                 <p>
                   <input
+                    // disabled={submited}
+                    onChange={handleFullnameInputChange}
+                    value={values.fullname}
                     type="text"
                     fullname="fullname"
                     className="fullname"
                     placeholder="Full Name"
                   />
+                  {submited && !values.fullname ? (
+                    <p className="warning">Please Enter your Full Name </p>
+                  ) : null}
                 </p>
                 <p>
                   <input
+                    // disabled={submited}
+                    onChange={handleEmailInputChange}
+                    value={values.email}
                     type="text"
                     email="email"
                     className="email"
                     placeholder="Email"
                   />
+                  {submited && !values.email ? (
+                    <p className="warning">Please Enter a Email Addres </p>
+                  ) : null}
                 </p>
                 <p>
                   <input
+                    // disabled={submited}
+                    onChange={handleMessageInputChange}
+                    value={values.message}
                     type="text"
                     message="message"
                     className="message"
                     placeholder="Message"
                   />
+                  {submited && !values.message ? (
+                    <p className="warning">Please Enter your Message </p>
+                  ) : null}
                 </p>
               </label>
+              <button type="submit" className="button">
+                submit
+              </button>
             </form>
           </div>
-          <button type="submit" className="button">
+          {/* <button type="submit" className="button" onSubmit={handleSubmit}>
             Submit
-          </button>
+          </button> */}
         </div>
       </div>
     </>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 //import React from "react";
 //import searchBox  from "../../components/SearchBox/searchBox";
+//import { React, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import background   from "../../images/book_stack.jpg";
 
-import background from "../../images/book_stack.jpg";
+
+//import background from "../../images/book_stack.jpg";
+
+//import background from "../../images/book_stack.jpg";
 import "./Home.css";
 
 export default function Home() {
@@ -11,8 +17,7 @@ export default function Home() {
         <div>
           <img src={background} alt="background" className="backgroundImage" />
         </div>
-
-        {/* <searchBox /> */}
+        {/* <div className="result-box"></div> */}
 
         <div className="searchBar">
           <form className="form">
@@ -26,7 +31,9 @@ export default function Home() {
               />
             </label>
             <div>
-              <button className="search">Search </button>
+              <button className="search">
+                <FaSearch size="30px" />
+              </button>
             </div>
           </form>
         </div>
