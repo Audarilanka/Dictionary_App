2e5192758f299816b4b634b3fbdef679cf1345a8
Audarilanka <dissanayakelanka96@gmail.com>
2022-07-20 09:45:30 +0530

    feat(project)dictionary app

---
+++ src/Data.js
@@ -0,0 +1,87 @@
+export const Data = [
+    {
+        "word":"A bed of roses",
+        "meaning":"a phrase used to reference a situation that is easy or comfortable",
+        "pic":""
+    },
+    {
+        "word":"A Priori",
+        "meaning":"something that is believed to be true without actual reinforcement with facts or statistics",
+        "pic":""
+    },
+    {
+        "word":"Aardvark",
+        "meaning":"a large, long-eared nocturnal animal that uses its long tongue to eat ants and termites",
+        "pic":""
+    },
+    {
+        "word":"Aback ",
+        "meaning":"in amazement; shocked",
+        "pic":""
+    },
+    {
+        "word":"Abaft",
+        "meaning":"toward the stern; behind",
+        "pic":""
+    },
+    {
+        "word":"Abandon",
+        "meaning":"to stop caring and leave or to desert",
+        "pic":""
+    },
+    {
+        "word":"Abase",
+        "meaning":"to lower so as to hurt feelings",
+        "pic":""
+    },
+    {
+        "word":"Abaser",
+        "meaning":"someone who degrades or lowers someone",
+        "pic":""
+    },
+    {
+        "word":"Abash",
+        "meaning":"to humiliate someone and make them feel embarrassed or ashamed",
+        "pic":""
+    },
+    {
+        "word":"Abate",
+        "meaning":"to reduce in degree or intensity",
+        "pic":""
+    },
+    {
+        "word":"Abatement",
+        "meaning":"reduction or weakening or something",
+        "pic":""
+    },
+    {
+        "word":"Abattoir",
+        "meaning":"a place where animals are taken to slaughter",
+        "pic":""
+    },
+    {
+        "word":"Abbess",
+        "meaning":"the female superior of a community of nuns",
+        "pic":""
+    },
+    {
+        "word":"Abbreviate",
+        "meaning":"to shorten a word or phrase",
+        "pic":""
+    },
+    {
+        "word":"Abdicate",
+        "meaning":"to stop accepting a particular responsibility or obligation that you have",
+        "pic":""
+    },
+    {
+        "word":"Abdominal",
+        "meaning":"related to the abdomen/stomach, or the part between the thorax and the pelvis",
+        "pic":""
+    },
+    {
+        "word":"Abduct",
+        "meaning":"to grab an individual by trickery or force",
+        "pic":""
+    }
+];
