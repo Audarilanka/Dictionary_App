1f63fafa5abebc8380ee10900fd0db32b4b2b17f
Audarilanka <dissanayakelanka96@gmail.com>
2022-07-22 23:40:52 +0530

    feat(project)dictionary

--- src/Data.js
+++ src/Data.js
@@ -1,92 +1,102 @@
+import sun from "./pictures/sun.jpg";
+import pen from "./pictures/pen.jpg";
+import dog from "./pictures/dog.jpg";
+import cat from "./pictures/cat.jpg";
+
 export const List = [
   {
-    word: "A bed of roses",
+    words: "A bed of roses",
     meaning:
       "a phrase used to reference a situation that is easy or comfortable",
-    pic: "",
+    //pic: { sun },
   },
   {
-    word: "A Priori",
+    words: "A Priori",
     meaning:
       "something that is believed to be true without actual reinforcement with facts or statistics",
     pic: "",
   },
   {
-    word: "Aardvark",
+    words: "Aardvark",
     meaning:
       "a large, long-eared nocturnal animal that uses its long tongue to eat ants and termites",
     pic: "",
   },
   {
-    word: "Aback ",
+    words: "Aback",
     meaning: "in amazement; shocked",
     pic: "",
   },
   {
-    word: "Abaft",
+    words: "Abaft",
     meaning: "toward the stern; behind",
     pic: "",
   },
   {
-    word: "Abandon",
+    words: "Abandon",
     meaning: "to stop caring and leave or to desert",
     pic: "",
   },
   {
-    word: "Abase",
+    words: "Abase",
     meaning: "to lower so as to hurt feelings",
     pic: "",
   },
   {
-    word: "Abaser",
+    words: "Abaser",
     meaning: "someone who degrades or lowers someone",
     pic: "",
   },
   {
-    word: "Abash",
+    words: "Abash",
     meaning: "to humiliate someone and make them feel embarrassed or ashamed",
     pic: "",
   },
   {
-    word: "Abate",
-    meaning: "to reduce in degree or intensity",
-    pic: "",
-  },
-  {
-    word: "Abatement",
-    meaning: "reduction or weakening or something",
-    pic: "",
-  },
-  {
-    word: "Abattoir",
-    meaning: "a place where animals are taken to slaughter",
-    pic: "",
-  },
-  {
-    word: "Abbess",
-    meaning: "the female superior of a community of nuns",
-    pic: "",
-  },
-  {
-    word: "Abbreviate",
-    meaning: "to shorten a word or phrase",
-    pic: "",
-  },
-  {
-    word: "Abdicate",
+    words: "Pen",
     meaning:
-      "to stop accepting a particular responsibility or obligation that you have",
-    pic: "",
+      "an instrument for writing or drawing with ink, typically consisting of a metal nib or ball, or a nylon tip, fitted into a metal or plastic holder.",
+    pic: { pen },
   },
   {
-    word: "Abdominal",
+    words: "Book",
     meaning:
-      "related to the abdomen/stomach, or the part between the thorax and the pelvis",
-    pic: "",
+      "a written or printed work consisting of pages glued or sewn together along one side and bound in covers.",
+    //pic: { book },
   },
   {
-    word: "Abduct",
-    meaning: "to grab an individual by trickery or force",
-    pic: "",
+    words: "Flower",
+    meaning:
+      "the seed-bearing part of a plant, consisting of reproductive organs (stamens and carpels) that are typically surrounded by a brightly coloured corolla (petals) and a green calyx (sepals).",
+    //pic: { flower },
+  },
+  {
+    words: "Tree",
+    meaning:
+      "a woody perennial plant, typically having a single stem or trunk growing to a considerable height and bearing lateral branches at some distance from the ground.",
+    //pic: { tree },
+  },
+  {
+    words: "Sun",
+    meaning: "the star round which the earth orbits.",
+    pic: { sun },
+  },
+  {
+    words: "Moon",
+    meaning:
+      "the natural satellite of the earth, visible (chiefly at night) by reflected light from the sun.",
+    //pic: { moon },
+  },
+  {
+    words: "Cat",
+    meaning:
+      "a small domesticated carnivorous mammal with soft fur, a short snout, and retractable claws. It is widely kept as a pet or for catching mice, and many breeds have been developed.",
+    pic: { cat },
+  },
+  {
+    words: "Dog",
+    meaning:
+      "a domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, non-retractable claws, and a barking, howling, or whining voice",
+    pic: { dog },
   },
 ];
