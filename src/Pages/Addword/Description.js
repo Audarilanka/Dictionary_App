0b48753b327af15d07062ab6076be8a0f913af6f
Audarilanka <dissanayakelanka96@gmail.com>
2022-08-15 00:13:33 +0530

    feat(project)add words

---
+++ src/Pages/Addword/Description.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Add.css";
+
+export default function Description() {
+  return (
+    <div className="Description-box">
+      <div>
+        <p>
+          Here you can both add new words and remove the ones already added to
+          it. Adding new words to our dictionary and removing your words from
+          dictionary is super simple. Whenever you want to add a new word to the
+          dictionary.To add a word, click 'Add' on the left. Then, scroll down
+          on the right and insert your word and it's meaning.To remove your
+          word,click 'Delete' on the left.
+        </p>
+        Here you can both add new words and remove the ones already added to it.
+        Adding new words to our dictionary and removing your words from
+        dictionary is super simple. Whenever you want to add a new word to the
+        dictionary.To add a word, click 'Add' on the left. Then, scroll down on
+        the right and insert your word and it's meaning.To remove your
+        word,click 'Delete' on the left.
+      </div>
+    </div>
+  );
+}
