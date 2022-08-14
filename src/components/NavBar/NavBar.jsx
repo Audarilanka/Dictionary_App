468bc4a1ccf12c08e56cad21fb1d924bc6196db1
Audarilanka <dissanayakelanka96@gmail.com>
2022-08-15 00:12:41 +0530

    feat(project)add words

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div>
-      <nav className="navbar">
+      <nav className="navbar1">
         <Link to="/" className="nav-links l1">
           <li className="nav-item dic">Dictionary</li>
         </Link>
@@ -19,6 +19,9 @@ export default function NavBar() {
         </Link>
         <Link to="/help" className="nav-links l4">
           <li className="nav-item">Help</li>
+        </Link>
+        <Link to="/add" className="nav-links l5">
+          <button className="nav-item add">ADD</button>
         </Link>
       </nav>
     </div>
